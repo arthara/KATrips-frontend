c8749add775ec83318b14d141ac7a2aedaf2f944
IntanFitri <intanfitrir1@gmail.com>
2020-11-13 11:38:05 +0700

    login_page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import LoginForm from "./containers/login";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="auth-wrapper">
+        <div className="auth-inner">
+          <LoginForm/>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/containers/login.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+import './login.css';
+
+import logo from '../assets/logo-fix.png';
+
+export default class login extends Component {
+
+    render() {
+        return (
+            <form>
+                return <img src={logo} class="center" alt="Logo" style={{height: "100px"}}/>;
+                
+                <div className="form-group">
+                    <input type="email" className="form-control" placeholder="Email" />
+                </div>
+
+                <div className="form-group">
+                    <input type="password" className="form-control" placeholder="Password" />
+                </div>
+
+                <button className="btn btn-primary btn-block">MASUK</button>
+                <button className="btn btn-link btn-block">DAFTAR AKUN</button>
+            </form>
+        )
+    }
+}
