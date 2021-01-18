da0e7725474779f6bb86b1899440cf61a15a7e08
nisarzz-UX <nisar.reus34@gmail.com>
2021-01-18 13:56:31 +0700

    Tambahan

--- src/Containers/HistoryPemesanan.js
+++ src/Containers/HistoryPemesanan.js
@@ -3,52 +3,91 @@ import { Link } from 'react-router-dom';
 import logoProduk from '../assets/logo-fix.png';
 import Gambar from '../assets/Picture1.svg';
 import Gambar2 from '../assets/Picture2.png';
+import axios from 'axios';
 import './HistoryPemesanan.css';
 
-function History(){
-        return(
-            <div className="bodyHistory">
-                        <div className="bgHistory">
-                        <ul>
-                            <img src={logoProduk} className="logo" alt="Logo" />
-                            <li><Link to ='/Login'>Sign out</Link></li>
-                        </ul>
-                
-                        <div className="historyPemesanan">Upcoming</div>
-                        
-                        <div class="kotakTengahUpcoming">
-                            <div class="bagianKiri">
-                                <p class = "kiri">User A <br/>
-                                    Kereta Jayabaya<br/>
-                                    Surabaya - Yogyakarta<br/>
-                                    Selasa, 20 Oktober 2020; 10.00 WIB
-                                </p>
-                            </div>
-                
-                            <div class="bagianKanan">
-                                <p class = "kanan">
-                                    Rp500.000 <img src={Gambar} className="gambar" alt="logo"></img>
-                                </p>
-                            </div>
-                        </div>
-                
-                        <div class="kotakTengahUpcomingMore">
-                            <div class="bagianKiri">
-                                <p class="kiri">
-                                    More Details
-                                </p>
-                            </div>
-                
-                            <div class="bagianKanan">
-                                <img src={Gambar2} className="more" alt="Logo"/>
-                            </div>
-                
-                        </div>
-                    
-                    
-             </div>
-        </div>
+//Halo
+export default class History extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+          perjalanan: []
+        };
+      }
+      
+      
+      
+      componentDidMount() {
+        axios.post(' http://api.katrips.me/api/user/cari-perjalanan',
+            {
+                lokasi_berangkat_id: 1,
+                lokasi_tiba_id: 2,
+                tanggal_berangkat: '2021-01-17',
+                jmlh_penumpang_dewasa: 1,
+                jmlh_penumpang_bayi: 0,
+                halo: 2,
+            },
+            {
+                headers: {
+                    Authorization: 'Bearer ' + localStorage.getItem('token')
+                }
+            }
         )
-}
 
-export default History;
+        .then
+        (res=>{
+            this.state.perjalanan = res.data.Perjalanan;
+        })
+        .catch(error=>{console.log(error)});
+      }
+
+    historyPemesanan = () =>{
+        let view = []
+        // Outer loop to create parent
+        console.log(this.state.perjalanan);
+        for (const value in this.state.perjalanan) {
+            view.push(<p class = "kiri"> <br/>
+            Harga : {value.harga} <br/>
+            Waktu Berangkat : {value.waktu_berangkat} <br/>
+            Waktu Tiba : {value.waktu_tiba} <br/>
+            </p>)
+        }
+        return view;
+    }
+      
+    render(){
+        return(
+                        <div className="bodyHistory">
+                                    <div className="bgHistory">
+                                    <ul>
+                                        <img src={logoProduk} className="logo" alt="Logo" />
+                                        <li><Link to ='/Login'>Sign out</Link></li>
+                                    </ul>
+                                    <div className="historyPemesanan">Upcoming</div>
+                                    <div class="kotakTengahUpcoming">
+                                        <div class="bagianKiri">
+                                            { this.historyPemesanan }
+                                        </div>
+                                        <div class="bagianKanan">
+                                            <p class = "kanan">
+                                                Rp500.000 <img src={Gambar} className="gambar" alt="logo"></img>
+                                            </p>
+                                        </div>
+                                    </div>
+                            
+                                    <div class="kotakTengahUpcomingMore">
+                                        <div class="bagianKiri">
+                                            <p class="kiri">
+                                                More Details
+                                            </p>
+                                        </div>
+                            
+                                        <div class="bagianKanan">
+                                            <img src={Gambar2} className="more" alt="Logo"/>
+                                        </div>  
+                                    </div>
+                         </div>
+                    </div>
+        )
+    }
+}
